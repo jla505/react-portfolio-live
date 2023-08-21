461381d93d2992b9d93118f5a63070619e5f5a53
Jin Liang <jinwoodjin@gmail.com>
2023-08-21 13:15:33 -0700

    Add components socialmediaicons

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,40 @@
+const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/jin-liang/"
+        target="_blank"
+        rel="noreferrer" // to make no problems with older browsers. copy and paste instead of using components IF NOT NEEDED. reusable component can take long time to write. dependent on specific use-case
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://twitter.com/_jimbojl"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="../assets/twitter.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com/jinliangjl/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="facebook-link" src="../assets/facebook.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com/_jimbojl/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="instagram-link" src="../assets/instagram.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
