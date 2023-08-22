6cd41274e6b00f05287de5173e3fce192caae823
Jin Liang <jinwoodjin@gmail.com>
2023-08-21 18:25:57 -0700

    Add github link to socialmediaicons

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -3,6 +3,14 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
+        href="https://github.com/jla505"
+        target="_blank"
+        rel="noreferrer" 
+      >
+        <img alt="github-link" src="../assets/github.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
         href="https://www.linkedin.com/in/jin-liang/"
         target="_blank"
         rel="noreferrer" // to make no problems with older browsers. copy and paste instead of using components IF NOT NEEDED. reusable component can take long time to write. dependent on specific use-case
